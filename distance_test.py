d1112fac7c739eb6f01941b5891cd057cfe5b86f
fiished <j.dolphin2013@gmail.com>
2017-07-27 17:19:52 +1000

    Threaded techniques implemented for multi sensor measurements

--- distance_test.py
+++ distance_test.py
@@ -18,7 +18,7 @@ def centroid(A, B, C):
     xco = float((1 - C ** 2 + A ** 2) / 2)
     yco = float((1 - B ** 2 + A ** 2) / 2)
 
-    print (xco, yco)
+    print(xco, yco)
     return xco, yco
 
 
@@ -28,7 +28,7 @@ while True:  # Continuous operation
     time.sleep(2)  # Delay of 2 seconds for easier readings
     array = []
     pool = ThreadPool(processes=4)
-    results_task = [pool.apply_async(dist_calc,()) for i in range(4)]
+    results_task = [pool.apply_async(dist_calc, ()) for i in range(4)]
 
     t1 = threading.Thread(target=dist_calc)
     t2 = threading.Thread(target=dist_calc)
@@ -41,9 +41,6 @@ while True:  # Continuous operation
     t4.start()
 
     results = list(map(lambda r: r.get(), results_task))
-
-
-
 
     results.sort()
     print(results)
